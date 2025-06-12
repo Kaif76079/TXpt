bec102c8666271440d05880b2812d24f141b7945
SUJAL CHAVDA <chavdasujal08@gmail.com>
2025-06-12 11:34:53 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,12 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "26330942"))
+API_HASH = environ.get("API_HASH", "5de9fd033aa828dfd3bf0c28adeee660")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7940644383:AAE39tAMMwWG7tPDQf1WDfwAPgRtFQMfHZg")
+OWNER = int(environ.get("OWNER", "6883471516"))
+CREDIT = "—͟͞͞★ន𝘶ᴊ𝒂𝒍—͟͞͞★"
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
